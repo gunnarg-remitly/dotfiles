47962d434cff08e4f5d2f5b4a5aac017d23bfb16
Gunnar Gissel <gunnarg@remitly.com>
2022-03-17 12:35:05 -0700

    more improvements as we go

--- functions/start-day.fish
+++ functions/start-day.fish
@@ -1,5 +1,3 @@
-# Defined in /var/folders/j5/wvy0_60111v2tsfmp0pvxvk80000gn/T//fish.5CcBZg/start-day.fish @ line 1
 function start-day
-    forge auth aws 
-    forge auth aws -p cstdev 
+    forge auth aws
 end
